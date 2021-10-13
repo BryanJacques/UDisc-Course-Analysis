3aa4ecf7fb4b7d4bca3f80c7a0b240a86dd262f1
BryanJacques <bryanjjacques@gmail.com>
2021-10-13 13:01:31 -0400

    create loop to get results of round

--- Course Analysis.R
+++ Course Analysis.R
@@ -3,16 +3,69 @@ library(tidyverse)
 library(here)
 library(readxl)
 
+rm(list = ls())
+
+# hard coded variables
+scores_lag <- 3                                                                 # amount of lag between time name appears in results data and when scores first appear, this is fixed
+hole_indicators <- c("MAP","VIEW MAP")                                          # Udisc uses these values to view hole maps, used to determine how many holes are on course
+results_file <- here("data/Results/WACO3.xlsx")
+
 # load top players
 top_players <- read_csv(here("data/Top Players.csv"))
 
 # load las vegas challenge
-Las_Vegas_Challenge <- read_excel(
-  here("data/Results/Las Vegas Challenge.xlsx")
-  ,col_names = "Value") %>% 
-  mutate(row_num = 1:length(Las_Vegas_Challenge$Value)) %>% 
-  left_join(top_players, by = c("Value" = "Player_Name"))
+LVCR <- read_excel(
+  results_file
+  ,col_names = "value") %>% 
+  filter(!is.na(`value`)) 
+
+# add fields, including has_space to detect if player name
+LVCR <- LVCR %>% 
+  mutate(
+    row_num = 1:n()
+    ,has_space = str_detect(`value`, " ")
+    )
+
+# get hole count for course
+hole_count <- LVCR %>% 
+  select(`value`) %>%
+  filter(str_to_upper(`value`) %in% hole_indicators) %>%
+  NROW()
+
+
+# get name rank
+name_rank <- LVCR %>% 
+  filter(has_space) %>% 
+  mutate(name_rank = 1:n()) %>% 
+  select(row_num, name_rank)
+
+# add name rank and dummy cols to LVCR
+LVCR <- LVCR %>% 
+  left_join(name_rank, by = "row_num") %>% 
+  mutate(hole = NA
+         ,score = NA
+         ,player = NA)
+
+
+# get scores
+for (item in 1:nrow(LVCR)) {
+  if (LVCR[item, "has_space"] == T) {
+    
+    row_num <- LVCR[item, "row_num"]
+    player <- LVCR[item, "value"]
+
+    for(hole in 1:hole_count) {
+      select_row <- as.integer(row_num + scores_lag + hole)
+      
+      LVCR[select_row, "hole"] <- hole
+      LVCR[select_row,"score"] <- LVCR[select_row,"value"]
+      LVCR[select_row,"player"] <- player
+    }
+    
+  }
+  
+}
 
 
 # view(top_players)
-# view(Las_Vegas_Challenge)
+ view(LVCR)
